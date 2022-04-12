0e6f026e74594cb3e471b73f1bd7331675022a46
adembasharon <adembasharon816@gmail.com>
2022-04-12 15:30:23 +0300

    modified cors in indeexjs

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.listen(PORT,()=>{
     console.log(`sever is running on ${PORT}`)
 });
 
-// app.use(cors({"Access-Control-Allow-Headers":"*"}));
+app.use(cors());
 
 app.get("/", (req, res)=>{
     res.send("Welcome to the API")
