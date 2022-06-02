c71c6947eb64dc20357769c024b69535297d7d60
adembasharon <adembasharon816@gmail.com>
2022-06-02 10:58:05 +0300

    modified nodemailer

--- routes/nodemailer.js
+++ routes/nodemailer.js
@@ -7,16 +7,17 @@ router.post("/booking",async (req , res)=>{
      host: "smtp.gmail.com",
      port: 587,
      auth: {
-       user: 'adembasharon816', 
-       pass:  'adwacznhkoqiyuwr', 
-       
+      type: "OAuth2",
+      clientId: "612894505463-tl05pd51li29007a9cg0hahmq1pig70m.apps.googleusercontent.com",
+      clientSecret: "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",
     },
+    
    });
  async function run() {
 
    let infoContent = await transporter.sendMail({
      from: `${req.body.name} <${req.body.email}>`, 
-     to: "adembasharon816@gmail.com", 
+     to: "lorinesotile@gmail.com", 
      subject:`${req.body.category}`, 
      text:`${req.body.message}`, 
      html:`<div><p>${req.body.name}<br/>${req.body.phonenumber}<br/>${req.body.message}</p></div>`,
